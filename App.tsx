fadacb6240ba6acc694f68c45f35df9a1541a35c
a-simplecode <amineamine19961996@gmail.com>
2023-01-24 12:23:15 +0200

    ADD login page simple design

--- App.tsx
+++ App.tsx
@@ -1,39 +1,51 @@
 import React, {useState} from 'react';
-import {
-  StyleSheet,
-  Text,
-  View,
-  TextInput,
-  FlatList,
-  Platform,
-} from 'react-native';
+import {StyleSheet, Text, View, TextInput, Button, Alert} from 'react-native';
 
 const PizzaTranslator = () => {
-  const [text, setText] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
   return (
-    <View style={styles.view}>
+    <View style={styles.container}>
+      <Text style={styles.title}>Login</Text>
       <TextInput
         style={styles.textInput}
-        placeholder="Type here to translate!"
-        onChangeText={newText => setText(newText)}
-        defaultValue={text}
+        placeholder="Username"
+        onChangeText={newText => setUsername(newText)}
+        defaultValue={username}
       />
-      <FlatList
-        data={text.split(' ').map(word => word && '🍕')}
-        renderItem={({item}) => <Text style={styles.text}>{item}</Text>}
+      <TextInput
+        style={styles.textInput}
+        placeholder="Password"
+        onChangeText={newText => setPassword(newText)}
+        defaultValue={password}
+      />
+      <Button
+        title="Submit"
+        disabled={!username || !password}
+        onPress={() => Alert.alert('welcome ' + username)}
       />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  view: {
-    paddingTop: Platform.OS === 'ios' ? 50 : 300,
-    paddingBottom: 50,
-    paddingLeft: 20,
-    paddingRight: 20,
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'column',
+    padding: 10,
   },
-  textInput: {height: 40},
-  text: {padding: 10, fontSize: 42},
+  title: {
+    fontSize: 30,
+    marginBottom: 20,
+  },
+  textInput: {
+    borderWidth: 1,
+    padding: 10,
+    margin: 10,
+    borderRadius: 10,
+    width: '70%',
+  },
 });
 export default PizzaTranslator;

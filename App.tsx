b4033c2d8e355294e3245ef2a77bda026f601078
a-simplecode <amineamine19961996@gmail.com>
2023-01-24 11:15:50 +0200

    FIX: prettier

--- App.tsx
+++ App.tsx
@@ -1,5 +1,12 @@
-import React, { useState } from "react";
-import { StyleSheet, Text, View, TextInput, FlatList, Platform } from 'react-native';
+import React, {useState} from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  FlatList,
+  Platform,
+} from 'react-native';
 
 const PizzaTranslator = () => {
   const [text, setText] = useState('');
@@ -13,19 +20,20 @@ const PizzaTranslator = () => {
       />
       <FlatList
         data={text.split(' ').map(word => word && '🍕')}
-        renderItem={({ item }) => <Text style={styles.text}>{item}</Text>} />
+        renderItem={({item}) => <Text style={styles.text}>{item}</Text>}
+      />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   view: {
-    paddingTop: Platform.OS === "ios" ? 50 : 300,
+    paddingTop: Platform.OS === 'ios' ? 50 : 300,
     paddingBottom: 50,
     paddingLeft: 20,
-    paddingRight: 20
+    paddingRight: 20,
   },
-  textInput: { height: 40 },
-  text: { padding: 10, fontSize: 42 }
+  textInput: {height: 40},
+  text: {padding: 10, fontSize: 42},
 });
 export default PizzaTranslator;
